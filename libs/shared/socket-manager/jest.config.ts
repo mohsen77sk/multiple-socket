8a89e3fcd4546e92aaece0b1957bacd32b6ecb0d
Mohsen-MSK <mohsen77sk@gmail.com>
2025-01-28 13:11:41 +0330

    update socket-manager

--- libs/shared/socket-manager/jest.config.ts
+++ libs/shared/socket-manager/jest.config.ts
@@ -1,5 +1,5 @@
 export default {
-  displayName: 'SocketManager',
+  displayName: 'socket-manager',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../../coverage/libs/shared/socket-manager',

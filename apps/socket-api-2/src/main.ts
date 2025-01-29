2cf6ea779e9488616e8fc12f9e1d9cf74903650a
mohsen77sk <mohsen77sk@gmail.com>
2025-01-29 09:26:03 +0330

    add broadcast messages to servers

--- apps/socket-api-2/src/main.ts
+++ apps/socket-api-2/src/main.ts
@@ -2,18 +2,27 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 
+const appName = 'Socket API 2';
 const app = express();
 const server = createServer(app);
 const port = process.env.PORT || 3002;
 const io = new Server(server, { transports: ['websocket'] });
 
 app.get('/', (req, res) => {
-  res.send('<p>Welcome to socket-api-2</p>');
+  res.send(`<p>Welcome to ${appName}</p>`);
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected to socket-api-2');
+  console.log(`a user connected to ${appName}`);
 });
+
+setInterval(() => {
+  const timestamp = new Date().toLocaleTimeString();
+  const message = `Broadcast message sent at ${timestamp} from ${appName}`;
+
+  io.emit('broadcastMessage', message);
+  console.log('Broadcast message sent:', message);
+}, 5000);
 
 server.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
